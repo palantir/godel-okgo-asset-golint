fbab2a5fe79828fa9b3123b4964dbbc77535afb9
Nick Miyake <nmiyake@users.noreply.github.com>
2018-04-25 16:04:57 -0700

    Update cobra CLI version (#10)

--- main.go
+++ main.go
@@ -32,7 +32,6 @@ func main() {
 
 func checkMain(osArgs []string) int {
 	os.Args = osArgs
-	var debugFlagVal bool
 	rootCmd := checker.AssetRootCmd(creator.Golint(), config.UpgradeConfig, "run golint check")
-	return cobracli.ExecuteWithDefaultParamsWithVersion(rootCmd, &debugFlagVal, "")
+	return cobracli.ExecuteWithDefaultParams(rootCmd)
 }
