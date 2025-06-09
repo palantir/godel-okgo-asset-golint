3a9e4cd45e1fe27fef7dc5bca31dadeaac36169e
Nick Miyake <nmiyake@palantir.com>
2025-06-09 14:12:12 -0700

    Set GOROOT if it is not set

--- main.go
+++ main.go
@@ -15,9 +15,7 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"runtime/pprof"
 
 	"github.com/palantir/amalgomate/amalgomated"
 	amalgomatedcheck "github.com/palantir/godel-okgo-asset-golint/generated_src"
@@ -27,55 +25,12 @@ import (
 	"github.com/palantir/pkg/cobracli"
 )
 
-const (
-	cpuProfilePublicFlagName  = "cpuprofile"
-	cpuProfilePrivateFlagName = "cpuprofile-private"
-)
-
 func main() {
-	os.Exit(runMain())
-}
-
-// runMain is the functional equivalent of the main function and returns an int exit code. Making this a separate
-// function ensures that the defer call is executed before the program exits (os.Exit does not run deferred functions).
-func runMain() int {
-	// if cpuProfilePrivateFlagName flag is present, profile
-	if cpuProfileFlagVal, osArgs := getFlagVal(cpuProfilePrivateFlagName, os.Args); cpuProfileFlagVal != "" {
-		f, err := os.Create(cpuProfileFlagVal)
-		if err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "Failed to create CPU profile: %v\n", err)
-			return 1
-		}
-		if err := pprof.StartCPUProfile(f); err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "Failed to start CPU profile: %v\n", err)
-			return 1
-		}
-		defer func() {
-			pprof.StopCPUProfile()
-			if err := f.Close(); err != nil {
-				_, _ = fmt.Fprintf(os.Stderr, "Failed to close CPU profile file: %v\n", err)
-			}
-		}()
-		os.Args = osArgs
-	}
-	return amalgomated.RunApp(os.Args, nil, amalgomated.NewCmdLibrary(amalgomatedcheck.Instance()), checkMain)
+	os.Exit(amalgomated.RunApp(os.Args, nil, amalgomated.NewCmdLibrary(amalgomatedcheck.Instance()), checkMain))
 }
 
 func checkMain(osArgs []string) int {
-	var cpuProfileFlagVal string
-	cpuProfileFlagVal, osArgs = getFlagVal(cpuProfilePublicFlagName, osArgs)
 	os.Args = osArgs
-	rootCmd := checker.AssetRootCmd(creator.Golint(cpuProfileFlagVal), config.UpgradeConfig, "run golint check")
+	rootCmd := checker.AssetRootCmd(creator.Golint(), config.UpgradeConfig, "run golint check")
 	return cobracli.ExecuteWithDefaultParams(rootCmd)
-}
-
-func getFlagVal(flagName string, osArgs []string) (flagVal string, osArgsWithFlagAndValRemoved []string) {
-	for idx, arg := range osArgs {
-		if arg == "--"+flagName && idx+1 < len(osArgs) {
-			flagVal = osArgs[idx+1]
-			osArgs = append(osArgs[:idx], osArgs[idx+2:]...)
-			break
-		}
-	}
-	return flagVal, osArgs
 }
